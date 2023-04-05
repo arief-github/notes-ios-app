3f86d2e894238793f0b241c850f6a64b0447774b
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-04-05 17:49:12 +0700

    fix little changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 import AddButton from "./AddButton/AddButton";
-import Body from "./Body/Body";
-import Footer from "./Footer/Footer";
 import  Header  from "./Header/Header";
 import ListItem from "./ListItem/ListItem";
 
-export { Header, Body, Footer, ListItem, AddButton };
+export { Header ,ListItem, AddButton };
